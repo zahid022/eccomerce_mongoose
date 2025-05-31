f54380bbf1646f8a26739e7e68b0c8f5a429145c
zahid022 <zahid.hzade001@gmail.com>
2025-05-31 15:44:51 +0400

    lat update

--- src/utils/jwt.utils.js
+++ src/utils/jwt.utils.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken")
 const config = require("./../config/index")
 
 const encode = (payload) => {
-    let token = jwt.sign(payload, config.secretKEY, {expiresIn : "1d"})
+    let token = jwt.sign(payload, config.secretKEY, {expiresIn : "1y"})
 
     return token
 }
